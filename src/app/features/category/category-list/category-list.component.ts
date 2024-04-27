a51736817002638e4a16be787be93d117bc73007
lakshmi <lakshmi@ACE-DE-0454>
2024-04-27 17:51:32 +0400

    oooooo

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit{
+
+  constructor(private categoryService :CategoryService)
+  {
+
+
+
+  }
+  ngOnInit(): void {
+this.categoryService.getAllCategories().subscribe;
+  }
 
 }
